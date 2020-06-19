f2b6d0a9c08feab71b059e7c366005bd8a17e9df
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-06-19 14:26:03 +0200

    erdtfyuhi

--- commands/color.js
+++ commands/color.js
@@ -14,7 +14,7 @@ module.exports = {
                 let theColorTheyWant = args[0];
                 if (!theColorTheyWant) return message.channel.send(`Please tell me what color you'd like.`);
                 if (!shop.roles[theColorTheyWant]) return message.channel.send(`That is not a valid color.`);
-                if (!res.unlocks.color.includes(theColorTheyWant)) return message.channel.send(`You do not have that color unlocked.`);
+                if (!res.unlocks.colors.includes(theColorTheyWant)) return message.channel.send(`You do not have that color unlocked.`);
                 let theRoleTheyWant = message.guild.roles.cache.get(shop.roles[theColorTheyWant].id);
                 switch (message.member.roles.cache.has(theRoleTheyWant.id)) {
                     case false:
